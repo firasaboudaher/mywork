c7fea2437155441ca20235cd227372b110f645cf
firasaboudaher <82384529+firasaboudaher@users.noreply.github.com>
2022-03-01 14:22:55 +0200

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import './Form.css';
+class Form extends Component {
+    state = {  } 
+    render() { 
+        return(
+            
+            <div className="parent">
+                <img src="images.png" alt="jswjb" className="i"/>
+                <h4 className="hh">Criminal Court of Appeal</h4>
+                <img src="karim.JPG" alt="jswjb" className="image"/>
+                
+            <div className="para">
+<b>
+            <div>- Within every governorate, there is one or more courts of appeal.  </div>
+<div>- The Court of Appeal consists of 3 judges: a president and two consultants</div>
+<div>- The public prosecution is represented before the Court of Appeal by the Appellate Public Prosecutor or an Attorney General.
+</div>
+</b>
+<hr className="sater"/>    </div>
+           <div className="juri"><b>Their jurisdiction:</b></div>
+           <hr className="sat"/>
+           <div><img src="download.png" alt="arrow" className="texas"/></div>
+           <img src="arrow.png" alt="arrow" className="arr"/>
+           <div className="spes">
+           <div><b>The Court of Appeal considers the appeal of all judgments issued by the Criminal Single Judge in the cases of misdemeanors, provided that they shall have been settled in the merits of the dispute.</b></div>
+            <div><b>However, if they were not settled in the merits of the dispute, they shall be appealed with the final judgment only.</b>
+</div>
+
+
+
+</div>
+<div className="pap"><b>With the exception of:</b></div>
+<hr className="s"/>  
+<div>
+<img src="arrow.png" alt="arrow" className="ar"/>
+    <div className="lahal"><b>The appeal of decisions that settle one or more of the formal defenses stipulated in Article</b></div>
+    <img src="arrow.png" alt="arrow" className="ar"/>
+    <div className="lahal"><b>Decisions by which the Single Judge terminates the lawsuit without addressing its subject matter</b></div>
+    <img src="arrow.png" alt="arrow" className="ar"/>
+    <div className="lahal"><b>Decisions issued on accepting or revoking the release requests</b></div>
+</div>
+<hr className="sater"/>
+<div>  <img src="search.png" alt="search" className="search"/>  
+<div className="sear"><b>The Court of Appeal considers the appeal of the judgments issued by the Criminal Single Judge in the cases of violations, provided that they shall be on:
+</b></div>
+</div>
+
+<div className="olo">
+<ol>
+  <li className="lol"><b>Imprisonment, 
+      preventive detention or a fine of more than five hundred thousand Lebanese pounds</b></li>
+  <li className="lolo"><b>Additional or secondary penalty or personal compensations of more than five hundred thousand Lebanese pounds</b></li>
+
+<li className="lolol"><b>Revocation of defense stipulated in Article 73 of the Criminal Procedures Code</b></li>
+<li className="lololo"><b>Penalty for a violation correlative with a misdemeanor</b></li>
+</ol> 
+<hr className="saters"/> 
+</div>
+
+<img src="pinn.jpeg" alt="pin" className="pin"/>
+<div className="pin2"><b>The Court of Appeal is entitled, when exercising its tasks, to listen to witnesses, interrogate the parties of the lawsuit, invite new witnesses, appoint an expert, listen to experts and carry out any new investigations it deems appropriate...
+</b></div>
+<div className="pin3"><b>Delay of appeal :</b></div>
+<hr className="sate"/> 
+
+<div className="final"><b>Final judgments:</b></div>
+<div className="wahad"><b>With respect to the plaintiff, defendant, responsible for money, and guarantor, the delay is 15 days:
+</b></div>
+<div className="tnen"><b>With respect to the Appellate Public Prosecutor, the delay is one month:
+</b></div>
+<div className="tlete"><b>With respect to the Appellate Public Prosecution, the delay is two months:
+</b></div>
+
+<img src="down.png" className="down1"/>
+<img src="down.png" className="down2"/>
+<img src="down.png" className="down3"/>
+
+<div className="la1"><b>As of the date of its issuance if it is made in presentia. As of the date of being notified whether it is made in presentia or absentia.
+</b></div>
+<div className="la2"><b>As of the date of issuance of the judgment</b></div>
+
+<div className="la3"><b>As of the date of issuance of the judgment</b></div>
+         
+         
+         <hr className="hayda"/>
+         <div className="lesh"><b>Decision on revoking or accepting the defenses stated in Article 73 of the Criminal Procedures Code:
+</b></div>
+<h1 className="h1">TABLE</h1>
+         <div className="table" >
+         <table>
+  <tr>
+    <th><b>With respect to the Public Prosecution, the delay is 24 hours :</b></th>
+    <th><b>With respect to the opponents, the delay is 24 hours :</b></th>
+    
+  </tr>
+  <tr>
+    <td>As of the date of issuance of the decision.</td>
+    <td>As of the date of notifying the opponents of the same.
+</td>
+    
+  </tr>
+ 
+</table>
+         </div>
+         <img src="ekher.png" className="ekher"/>
+<div className="khalas"><b>Decisions of the Court of Appeal:</b></div>
+
+
+<img src="ekher2.jpeg" className="ekher2"/>
+         
+
+<div className="khalas2"><b>Dissolution of the appealed judgment.</b></div>
+
+<div className="khalas3"><b>Legalization of the appealed judgment</b></div>
+
+<hr className="finish"/>
+
+
+            </div>
+            
+        );
+   
+    }
+}
+ 
+export default Form;
